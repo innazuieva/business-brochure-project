3b01576fe28f03913d905277882914d6176c9571
Zuieva Inna <zuevainna94@gmail.com>
2019-07-02 19:41:47 -0700

    Added home page styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,31 @@
+// Hamburger Menu 
+
 const body = document.body;
 const menu = document.getElementById('menu');
 
 menu.addEventListener('click', openMenu);
 
 function openMenu() {
-    console.log("hello")
     body.classList.toggle('show');
 }
 
 menu.addEventListener('mousedown', function (e) {
     e.preventDefault();
 });
+
+
+
+// Live Chat
+
+const chatButton = document.getElementById('live-chat-icon');
+const chatForm = document.getElementById('live-chat-form');
+
+chatButton.addEventListener('click', openChat);
+
+function openChat() {
+    console.log('hello')
+    chatForm.classList.toggle('showChat');
+}
+
+
+// Home Page Slideshow
